13156dda0649ab8dd6d7d504e43c68a54034c03b
Urika Pye <urikapyearies@gmail.com>
2020-01-11 18:42:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,3 +26,13 @@ song.name
 song = Song.creat_by_name("The Middle")
 song
 Song.all.include?(song)
+
+the_middle = Song.create_by_name("The Middle")
+Song.find_by_name("The Middle")
+
+song_1 = Song.find_or_create_by_name("Blank Space")
+song_2 = Song.find_or_create_by_name("Blank Space")
+song_1 == song_2
+
+song = Song.new_from_filename("Taylor Swift - Blank Space.mp3")
+song.namesong.artist_name
